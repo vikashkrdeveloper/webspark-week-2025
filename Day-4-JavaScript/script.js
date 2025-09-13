3b5c035df2a107888e7a6589b49d706a0461e096
Vikash Kumar <vikashjjp728@gmail.com>
2025-09-13 20:51:04 +0530

    Publish the workshop class source code and assignments.

---
+++ Day-4-JavaScript/script.js
@@ -0,0 +1,62 @@
+// var numbers=10;
+
+// var numbers="Hey";
+
+// let numbers = 10
+// let numbers = "hdsj"
+
+// const  numbers=10;
+// numbers=100
+
+// let  numbers=10;
+// numbers=100
+
+// console.log(numbers)
+// console.log(number)
+
+
+let strings = "Hello"
+let numbers = 10.2
+let booleans = true
+let nulls = null
+let undefineds = undefined
+let arrays = [1, 2, "k", 52.2]
+let objects = {
+    keys: "value"
+}
+
+strings = "10.2"
+// console.log(numbers, strings)
+// console.log(typeof numbers, typeof strings)
+// console.log("==",numbers==strings)
+// console.log("===",numbers===strings)
+// console.log(typeof strings)
+// console.log(numbers)
+// console.log(booleans)
+// console.log(nulls)
+// console.log(undefineds)
+// console.log(arrays)
+// console.log(objects)
+
+
+// console.log(strings === numbers ? "False" : "True")
+
+// function greet(name) {
+//     return "Hello, " + name;
+// }
+
+
+// console.log(greet("Vikah"))
+
+// const greet = (name) => {
+//     return "Hello, " + name;
+// }
+
+// console.log(greet("Vikah"))
+
+const changeNameButton=document.getElementById("change-name");
+changeNameButton.addEventListener('click',()=>{
+    
+//    console.log("Clicked") 
+})
+console.log(changeNameButton)
